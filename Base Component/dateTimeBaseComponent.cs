2b251b5feb4103179fadf6cd2153decc2bc29b77
Ramazan <codmyhesap@gmail.com>
2021-12-30 06:02:48 +0300

    Usa ve Jpn arasinda yapilan kucuk bir savas oyunu yazdik.

---
+++ Base Component/dateTimeBaseComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace vsCodePersonelBlazorApp.BaseComponent
+{
+    public class dateTimeBaseComponent : ComponentBase
+    {
+        public DateTime dateTime { get; set; }
+
+        public string GetDateTime()
+        {
+            dateTime = DateTime.Now;
+            return dateTime.ToString("U");
+        }
+    } 
+    
+}
+
